42426280ec1c20f8c28ab545fd54ee73610add1c
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-01 19:13:22 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Home/>
+      <Router>
+        <Routes>
+
+          <Route path='/' element={[<Header/>,<Home/>]}/>
+
+          <Route path='/checkout' element={[<Header/>]}/>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div className='home'>
 
             <div className="home__container">
-                <img className='home__image' alt="hero image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg" />
+                <img className='home__image' alt="hero" src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg" />
 
                 <div className="home__row">
                     <Product title="The lean startup: How constant Innovation Creates Radically Successful Businesses Paerback" price={29.99} rating={5} image="https://www.adorebooks.in/wp-content/uploads/2022/02/Untitled-design-2022-03-08T004033.655-800x800.jpg" />
