87c57c533121630ecd07bc3a97dd394b9209219d
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-01 18:38:40 +0530

    added

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -12,8 +12,14 @@ const Product = () => {
                     <small>$</small>
                     <strong>11.96</strong>
                 </p>
-                
+                <div className="product__rating">
+                    <p>🌟🌟🌟</p>
+                </div>
             </div>
+
+            <img src="https://www.adorebooks.in/wp-content/uploads/2022/02/Untitled-design-2022-03-08T004033.655-800x800.jpg" alt="startup" />
+
+            <button>Add to Basket</button>
 
         </div>
     )
