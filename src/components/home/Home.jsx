32afbab3a605bc18626202b1f74ce2f1cf78566a
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-01 18:27:48 +0530

    added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,6 +7,19 @@ function Home() {
 
             <div className="home__container">
                 <img className='home__image' alt="hero image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg" />
+
+                <div className="home__row">
+
+                </div>
+
+                <div className="home__row">
+
+                </div>
+                
+                <div className="home__row">
+
+                </div>
+
             </div>
 
         </div>
