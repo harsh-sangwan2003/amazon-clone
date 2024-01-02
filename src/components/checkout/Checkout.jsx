2ac303542fba049419c870a71686fa4213eff5fb
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-02 18:01:37 +0530

    added

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -3,7 +3,14 @@ import './Checkout.css'
 
 const Checkout = () => {
     return (
-        <div>
+        <div className='checkout'>
+
+            <div className="checkout__left">
+                <img src="https://paxcom.ai/wp-content/uploads/2016/06/banner.png" className='checkout__ad' alt="" />
+            </div>
+            <div className="checkout__right">
+                <h2 className='checkout__title'>Your shopping Basket</h2>
+            </div>
 
         </div>
     )
